78a3fac1b36723234b25a5d692b3ce5bc3b0cc84
ildysilva <ildeberto34@gmail.com>
2024-01-22 21:20:07 +0100

    doc: flutter moving background comments,documentation

--- lib/flutter_moving_background.dart
+++ lib/flutter_moving_background.dart
@@ -1,17 +1,33 @@
+/// A Flutter library for creating a moving background with customizable circles.
 library flutter_moving_background;
-export  'components/circle.dart';
 
+export 'components/circle.dart';
 
 import 'package:flutter/material.dart';
 
 import 'components/circle.dart';
 
+/// A widget representing a moving background with customizable circles.
 class MovingBackground extends StatelessWidget {
-  const MovingBackground(
-      {super.key, this.child, this.backgroundColor, required this.circles});
+  /// Creates a [MovingBackground] widget.
+  ///
+  /// The [child] is an optional widget that can be placed on top of the moving background.
+  /// The [backgroundColor] is the color of the background, defaulting to white if not provided.
+  /// The [circles] is a list of [MovingCircle] widgets that define the circles on the background.
+  const MovingBackground({
+    Key? key,
+    this.child,
+    this.backgroundColor,
+    required this.circles,
+  }) : super(key: key);
 
+  /// The child widget to be placed on top of the moving background.
   final Widget? child;
+
+  /// The color of the background. If not provided, defaults to white.
   final Color? backgroundColor;
+
+  /// List of [MovingCircle] widgets that define the circles on the background.
   final List<MovingCircle> circles;
 
   @override
@@ -20,8 +36,11 @@ class MovingBackground extends StatelessWidget {
       color: backgroundColor ?? Colors.white,
       child: Stack(
         children: [
+          // Place each circle from the list on the background.
           for (MovingCircle circle in circles) circle,
-          child ?? const SizedBox()
+
+          // Place the optional child widget on top of the background.
+          child ?? const SizedBox(),
         ],
       ),
     );
--- lib/painters/circles_painter.dart
+++ lib/painters/circles_painter.dart
@@ -1,31 +1,47 @@
 import 'package:flutter/material.dart';
 
+/// Custom painter class for drawing a circle with animation.
 class CirclesPainter extends CustomPainter {
+  /// The current value of the animation.
   final double animationValue;
 
+  /// The color of the circle.
   Color color;
+
+  /// The sigma value for the blur effect.
   double blurSigma;
 
-  CirclesPainter(this.animationValue,
-      {required this.color, this.blurSigma = 30});
+  /// Constructor for the CirclesPainter.
+  ///
+  /// The [animationValue] represents the current value of the animation (0.0 to 1.0).
+  /// The [color] is the color of the circle, and [blurSigma] is the sigma value for the blur effect.
+  CirclesPainter(this.animationValue, {required this.color, this.blurSigma = 30});
 
   @override
   void paint(Canvas canvas, Size size) {
+    // Create a paint object with the specified color and style.
     final Paint paint = Paint()
       ..color = color
       ..style = PaintingStyle.fill;
+
+    // Calculate the radius of the circle based on the width of the provided size.
     final double radius = size.width / 2;
 
+    // Calculate the x-coordinate offset based on the animation value.
     final double xOffset = size.width * animationValue;
-    final Offset center = Offset(xOffset, size.height / 100);
+    final Offset center = Offset(xOffset, size.height / 2);
 
+    // Apply a blur effect to the paint object.
     paint.maskFilter = MaskFilter.blur(BlurStyle.normal, blurSigma);
 
+    // Draw the circle on the canvas at the specified center with the calculated radius.
     canvas.drawCircle(center, radius, paint);
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
+    // Return false to indicate that the painting should not be repainted
+    // unless the properties of this painter change.
     return false;
   }
 }
